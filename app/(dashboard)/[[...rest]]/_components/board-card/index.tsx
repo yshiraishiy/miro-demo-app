066c215511efd2c5221e04ae13a58eadb864a561
yshiraishiy <yuya0505soccer@gmail.com>
2024-10-24 17:15:11 +0900

    ✨カードのドロップダウンメニューとコピー機能を追加

--- app/(dashboard)/[[...rest]]/_components/board-card/index.tsx
+++ app/(dashboard)/[[...rest]]/_components/board-card/index.tsx
@@ -6,7 +6,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { Overlay } from "./overlay";
 import { useAuth } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
 import { Footer } from "./footer";
+import { Actions } from "@/components/actions";
 
 interface BoardcardProps {
   id: string;
@@ -46,6 +48,17 @@ export const BoardCard = ({
           className="object-fit"
           />
           <Overlay />
+          <Actions 
+          id={id}
+          title={title}
+          side="right"
+          >
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal 
+              className="text-white opacity-75 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </Actions>
         </div>
         <Footer
         isFavorite={isFavorite}
