655f4a5f079fba7d601db68b83ce3636f1dbadd5
yshiraishiy <yuya0505soccer@gmail.com>
2024-10-25 13:13:47 +0900

    ✨カードの削除機能の実装

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { ConfirmModal } from "./confirm-modal";
 
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,8 +11,10 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { api } from "@/convex/_generated/api";
+import { Button } from "./ui/button";
 
 interface ActionProps {
   children: React.ReactNode;
@@ -27,11 +31,19 @@ export const Actions = ({
   id,
   title
 }: ActionProps) => {
+  const {mutate, pending} = useApiMutation(api.board.remove);
+
   const onCopyLink = () => {
     navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
       .then(() => toast.success("Link copied"))
       .catch(() => toast.error("Failed to copy link"))
   }
+
+  const onDelete = () => {
+    mutate({id})
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
 
   return (
     <DropdownMenu>
@@ -51,6 +63,20 @@ export const Actions = ({
           <Link2 className="h-4 w-4 mr-2"/>
           Copy board link
         </DropdownMenuItem>
+        <ConfirmModal
+        header="Delete board?"
+        description="This will delete the board and all of its contents."
+        disabled={pending}
+        onConfirm={onDelete}
+        >
+        <Button
+        variant="ghost"
+        className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+        >
+          <Trash2 className="h-4 w-4 mr-2"/>
+          Delete
+        </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
