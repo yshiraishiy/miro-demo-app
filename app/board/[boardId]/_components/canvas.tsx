0c0732e7e435a6e386306b4c3776e530beb0e8f3
yshiraishiy <yuya0505soccer@gmail.com>
2024-10-30 15:30:16 +0900

    ✨ツールバーの状態管理実装

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useState } from "react";
+
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { useHistory, useCanUndo, useCanRedo } from "@liveblocks/react/suspense";
 
 interface CanvasProps {
   boardId: string;
@@ -11,11 +16,26 @@ interface CanvasProps {
 export const Canvas = ({
   boardId
 }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None
+  })
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId}/>
       <Participants />
-      <Toolbar />
+      <Toolbar 
+      canvasState={canvasState}
+      setCanvasState={setCanvasState}
+      canRedo={canRedo}
+      canUndo={canUndo}
+      undo={history.undo}
+      redo={history.redo}
+      />
     </main>
   );
 };
